11d003537f37c61b2ce4736728e4d3178f06380f
Kihong <kks4517@naver.com>
2024-08-06 14:13:12 +0900

    ✨ Feature: Kakao OAuth2 관련 코드 추가
    OAuth2PRoviderRegistry를 통한 Provider 구분
    추후 Provider 확장을 대비한 Google, Naver 관련 코드 추가

---
+++ backend/src/main/java/com/pop/backend/global/security/auth/OAuth2UserAttributes.java
@@ -0,0 +1,85 @@
+package com.pop.backend.global.security.auth;
+
+import com.pop.backend.domain.user.entity.User;
+import com.pop.backend.global.security.auth.OAuth2ProviderRegistry.OAuth2ProviderType;
+import com.pop.backend.global.type.ProviderType;
+import java.util.Map;
+
+public record OAuth2UserAttributes(
+    Map<String, Object> attributes,
+    ProviderType providerType,
+    String email,
+    String nickname,
+    String image
+) {
+
+  public static OAuth2UserAttributes of(OAuth2ProviderType providerType, Map<String, Object> attributes) {
+    switch (providerType) {
+      case KAKAO -> {
+        return fromKakao(providerType, attributes);
+      }
+      case NAVER -> {
+        return fromNaver(providerType, attributes);
+      }
+      case GOOGLE -> {
+        return fromGoogle(providerType, attributes);
+      }
+      default -> throw new IllegalArgumentException("Unsupported provider type: " + providerType);
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  private static OAuth2UserAttributes fromKakao
+      (OAuth2ProviderType providerType, Map<String, Object> attributes) {
+    Map<String, Object> kakaoAttributes = extractAttributes(providerType.ATTRIBUTES_FIELD, attributes);
+    String email = (String) kakaoAttributes.get(providerType.EMAIL_FIELD);
+
+    kakaoAttributes = (Map<String, Object>) kakaoAttributes.get("profile");
+    return new OAuth2UserAttributes(
+        attributes,
+        ProviderType.find(providerType.name()),
+        email,
+        (String) kakaoAttributes.get(providerType.NICKNAME_FIELD),
+        (String) kakaoAttributes.get(providerType.IMAGE_FILED)
+    );
+  }
+
+  private static OAuth2UserAttributes fromNaver
+      (OAuth2ProviderType providerType, Map<String, Object> attributes) {
+    Map<String, Object> naverAttributes = extractAttributes(providerType.ATTRIBUTES_FIELD, attributes);
+    return new OAuth2UserAttributes(
+        attributes,
+        ProviderType.find(providerType.name()),
+        (String) naverAttributes.get(providerType.EMAIL_FIELD),
+        (String) naverAttributes.get(providerType.NICKNAME_FIELD),
+        (String) naverAttributes.get(providerType.IMAGE_FILED)
+    );
+  }
+
+  private static OAuth2UserAttributes fromGoogle
+      (OAuth2ProviderType providerType, Map<String, Object> attributes) {
+    return new OAuth2UserAttributes(
+        attributes,
+        ProviderType.find(providerType.name()),
+        (String) attributes.get(providerType.EMAIL_FIELD),
+        (String) attributes.get(providerType.NICKNAME_FIELD),
+        (String) attributes.get(providerType.IMAGE_FILED)
+    );
+  }
+
+  @SuppressWarnings("unchecked")
+  private static Map<String, Object> extractAttributes(String ATTRIBUTES_FIELD, Map<String, Object> attributes) {
+    return (Map<String, Object>) attributes.get(ATTRIBUTES_FIELD);
+  }
+
+  public User toUser(String password) {
+    return User.builder()
+               .email(email)
+               .nickname(nickname)
+               .profileImage(image)
+               .password(password)
+               .providerType(providerType)
+               .build();
+  }
+
+}
---
+++ backend/src/main/java/com/pop/backend/global/security/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.pop.backend.global.security.config;
+
+import com.pop.backend.global.security.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+  private final CustomOAuth2UserService oAuth2UserService;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    http.csrf(AbstractHttpConfigurer::disable)
+        .cors(AbstractHttpConfigurer::disable)
+        .formLogin(AbstractHttpConfigurer::disable);
+
+    http.sessionManagement(sessionConfigurer -> sessionConfigurer
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+    http.authorizeHttpRequests(requestRegistry -> requestRegistry
+        .anyRequest().permitAll());
+
+    http.oauth2Login(oauth2LoginConfigurer -> oauth2LoginConfigurer
+        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+            .userService(oAuth2UserService)));
+
+    return http.build();
+  }
+
+}
