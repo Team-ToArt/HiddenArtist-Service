493cdbad93085ac0d95e7e9a9f5998ad7ec429fb
Kihong <kks4517@naver.com>
2024-08-07 23:26:38 +0900

    ✨ Feature: SecurityExceptionHandleFilter 추가
    GlobalExceptionHandler에서 예외 로깅까지 수행하도록 수정
    SecurityFilter에 대한 예외를 SecurityExceptionHandleFilter에서
    처리하도록 구현

--- backend/src/main/java/com/pop/backend/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/pop/backend/global/exception/GlobalExceptionHandler.java
@@ -3,15 +3,18 @@ package com.pop.backend.global.exception;
 import com.pop.backend.global.exception.response.ErrorResponse;
 import com.pop.backend.global.exception.response.ServiceErrorResponse;
 import com.pop.backend.global.exception.type.ServiceException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(value = {ServiceException.class})
   public ResponseEntity<ErrorResponse> handleServiceException(ServiceException ex) {
+    log.error("{} \n {}", ex.getErrorDetail().getMessage(), ex.getStackTrace()[0]);
     ErrorResponse errorResponse = new ServiceErrorResponse(ex);
     return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
   }
