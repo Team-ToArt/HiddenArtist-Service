fa7eb1544a1893eb77fb14001b6f0035650a1730
Kihong <kks4517@naver.com>
2024-08-07 22:37:59 +0900

    ✨ Feature: 예외발생 로깅 AOP 구현
    AOP를 사용해 예외 발생 로깅 처리
    SecurityFilter는 AOP 범위에서 제외

---
+++ backend/src/main/java/com/pop/backend/global/exception/ExceptionLoggingAspect.java
@@ -0,0 +1,23 @@
+package com.pop.backend.global.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class ExceptionLoggingAspect {
+
+  @Pointcut("execution(* com.pop.backend..*(..)) && !within(com.pop.backend.global.security.filter..*)")
+  private void exceptionLoggingPointcut() {
+  }
+
+  @AfterThrowing(pointcut = "exceptionLoggingPointcut()", throwing = "ex")
+  public void loggingServiceException(ServiceException ex) {
+    log.error("{} \n {}", ex.getErrorDetail().getMessage(), ex.getStackTrace()[0]);
+  }
+
+}
