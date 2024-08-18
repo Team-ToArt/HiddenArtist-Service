4e9b04a2ae26a967d9405b6ddff6ac2d75adee46
Kihong <kks4517@naver.com>
2024-08-18 13:49:43 +0900

    ♻️ Refactor: Test 설정파일 분리
    Test 설정파일을 Test디렉토리 하위에 위치하도록 수정

--- backend/src/test/java/com/pop/backend/global/CustomDataJpaTest.java
+++ backend/src/test/java/com/pop/backend/global/CustomDataJpaTest.java
@@ -6,14 +6,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @DataJpaTest
 @Transactional
-@ActiveProfiles("test")
 @Import(TestConfig.class)
 public @interface CustomDataJpaTest {
 
