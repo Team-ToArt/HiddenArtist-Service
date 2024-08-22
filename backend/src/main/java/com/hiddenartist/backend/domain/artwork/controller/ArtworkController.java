4b00f0cf66d7c1445fc0dadccba94779b1bea5eb
Kihong <kks4517@naver.com>
2024-08-22 20:05:35 +0900

    ✨ Feature: 작품 Pick 기능 구현
    Issues: #18

--- backend/src/main/java/com/hiddenartist/backend/domain/artwork/controller/ArtworkController.java
+++ backend/src/main/java/com/hiddenartist/backend/domain/artwork/controller/ArtworkController.java
@@ -4,8 +4,10 @@ import com.hiddenartist.backend.domain.artwork.controller.response.ArtworkGetDet
 import com.hiddenartist.backend.domain.artwork.controller.response.ArtworkGetRecommendResponse;
 import com.hiddenartist.backend.domain.artwork.service.ArtworkService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,11 @@ public class ArtworkController {
   @GetMapping("/recommend")
   public ArtworkGetRecommendResponse getArtworkRecommend() {
     return artworkService.getArtworkRecommend();
+  }
+
+  @PostMapping("/{token}/pick")
+  public void savePickArtwork(@AuthenticationPrincipal String email, @PathVariable("token") String token) {
+    artworkService.savePickArtwork(email, token);
   }
 
 }
--- backend/src/main/java/com/hiddenartist/backend/domain/artwork/service/ArtworkService.java
+++ backend/src/main/java/com/hiddenartist/backend/domain/artwork/service/ArtworkService.java
@@ -1,9 +1,15 @@
 package com.hiddenartist.backend.domain.artwork.service;
 
+import com.hiddenartist.backend.domain.account.persistence.Account;
+import com.hiddenartist.backend.domain.account.persistence.repository.AccountRepository;
 import com.hiddenartist.backend.domain.artwork.controller.response.ArtworkGetDetailResponse;
 import com.hiddenartist.backend.domain.artwork.controller.response.ArtworkGetRecommendResponse;
 import com.hiddenartist.backend.domain.artwork.persistence.Artwork;
+import com.hiddenartist.backend.domain.artwork.persistence.PickArtwork;
 import com.hiddenartist.backend.domain.artwork.persistence.repository.ArtworkRepository;
+import com.hiddenartist.backend.domain.artwork.persistence.repository.PickArtworkRepository;
+import com.hiddenartist.backend.global.exception.type.EntityException;
+import com.hiddenartist.backend.global.exception.type.ServiceErrorCode;
 import com.hiddenartist.backend.global.type.EntityToken;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +21,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class ArtworkService {
 
   private final ArtworkRepository artworkRepository;
-
+  private final AccountRepository accountRepository;
+  private final PickArtworkRepository pickArtworkRepository;
 
   @Transactional(readOnly = true)
   public ArtworkGetDetailResponse getArtworkDetail(String tokenValue) {
@@ -27,6 +34,17 @@ public class ArtworkService {
   public ArtworkGetRecommendResponse getArtworkRecommend() {
     List<Artwork> artworkRecommend = artworkRepository.findArtworkRecommend();
     return ArtworkGetRecommendResponse.create(artworkRecommend);
+  }
+
+  @Transactional
+  public void savePickArtwork(String email, String tokenValue) {
+    String token = EntityToken.ARTWORK.identifyToken(tokenValue);
+    Account account = accountRepository.findByEmail(email)
+                                       .orElseThrow(() -> new EntityException(ServiceErrorCode.USER_NOT_FOUND));
+    Artwork artwork = artworkRepository.findByToken(token)
+                                       .orElseThrow(() -> new EntityException(ServiceErrorCode.ARTWORK_NOT_FOUND));
+    PickArtwork pickArtwork = PickArtwork.builder().account(account).artwork(artwork).build();
+    pickArtworkRepository.save(pickArtwork);
   }
 
 }
