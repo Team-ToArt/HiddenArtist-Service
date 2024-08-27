d5c29c92e825c2b28fa00864a5b559cedadf744b
Kihong <kks4517@naver.com>
2024-08-27 12:20:10 +0900

    💬 Feature: 스켈레톤 코드 작성중

--- backend/src/main/java/com/hiddenartist/backend/domain/admin/controller/AdminPageController.java
+++ backend/src/main/java/com/hiddenartist/backend/domain/admin/controller/AdminPageController.java
@@ -2,6 +2,9 @@ package com.hiddenartist.backend.domain.admin.controller;
 
 import com.hiddenartist.backend.domain.admin.type.PageInfo;
 import java.util.Map;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +25,24 @@ public class AdminPageController {
   public String routeLoginPage(Model model) {
     Map<String, String> attributes = PageInfo.LOGIN.getAttributes();
     model.addAllAttributes(attributes);
+    return "index";
+  }
+
+  // 작가 조회
+  @GetMapping("/artists")
+  public String routeArtistsPage(
+      @PageableDefault(page = 1, sort = {"name", "id"}, direction = Direction.ASC) Pageable pageable, Model model
+  ) {
+    // 페이징 처리해서 작가 조회
+    return "index";
+  }
+
+  // 작품 조회
+  @GetMapping("/artworks")
+  public String routeArtworksPage(
+      @PageableDefault(page = 1, sort = {"name", "id"}, direction = Direction.ASC) Pageable pageable, Model model
+  ) {
+    // 페이징 처리해서 작품 조회
     return "index";
   }
 
