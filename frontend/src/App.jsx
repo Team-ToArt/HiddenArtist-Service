d23a2dc08496f28ee22cf9c6b85e5d5bbaa4aedf
kikiu90 <kikiu90@naver.com>
2024-10-05 18:32:23 +0900

    ✨ Feature: 홈 및 전시회 UI 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, Route, Routes } from "react-router-dom";
+import Header from './Header.jsx'
+import Home from './Home.jsx'
+import Artist from './Artist.jsx'
+import Artworks from './Artworks.jsx'
+import Exhibition from './Exhibition.jsx'
+import ExhDetail from './ExhDetail.jsx'
+import Mentoring from './Mentoring.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      
+      <Routes>
+        <Route element={<Header />}>
+          <Route path="" element={<Home />} />
+          <Route path="artist" element={<Artist />} />
+          <Route path="artworks" element={<Artworks />} />
+          <Route path="exhibition" element={<Exhibition />} />
+          <Route path="exhibition/:exhID" element={<ExhDetail />} />
+          <Route path="mentoring" element={<Mentoring />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
-
-export default App
+export default App;
---
+++ frontend/src/Artworks.jsx
@@ -0,0 +1,39 @@
+import './assets/css/Home.css';
+
+function App() {
+
+  return (
+    <>
+    <div style={{marginTop: '30px', display:'flex', flexDirection:'column', alignItems:'center'}}>
+        <div style={{marginBottom:'62px'}}>
+            <h2>작가</h2> 
+            <div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+                <div class="img_art"><img src="https://via.placeholder.com/356x356" alt=""/></div>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default App
