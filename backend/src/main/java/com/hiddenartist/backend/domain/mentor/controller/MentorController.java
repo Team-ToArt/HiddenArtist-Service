20892a7678b0e6ca11e1fbf30d434360689fadb1
Kihong <kks4517@naver.com>
2024-10-05 15:05:36 +0900

    ✨ Feature: Mentoring 신청내역 조회(Mentor) 구현
    Issues: #26

--- backend/src/main/java/com/hiddenartist/backend/domain/mentor/controller/MentorController.java
+++ backend/src/main/java/com/hiddenartist/backend/domain/mentor/controller/MentorController.java
@@ -1,7 +1,9 @@
 package com.hiddenartist.backend.domain.mentor.controller;
 
+import com.hiddenartist.backend.domain.mentor.controller.response.ReceivedMentoringApplicationResponse;
 import com.hiddenartist.backend.domain.mentor.service.MentorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
@@ -18,11 +20,11 @@ public class MentorController {
   private final MentorService mentorService;
 
   @GetMapping("/me/mentorings")
-  public void getReceivedMentoringApplications(
+  public Page<ReceivedMentoringApplicationResponse> getReceivedMentoringApplications(
       @PageableDefault(size = 20, page = 1, sort = "applicationTime", direction = Direction.DESC) Pageable pageable,
       @AuthenticationPrincipal String email
   ) {
-    // 신청받은 멘토링 내역 조회
+    return mentorService.getReceivedMentoringApplications(pageable, email);
   }
 
 }
--- backend/src/main/java/com/hiddenartist/backend/domain/mentor/persistence/repository/MentorRepository.java
+++ backend/src/main/java/com/hiddenartist/backend/domain/mentor/persistence/repository/MentorRepository.java
@@ -3,6 +3,6 @@ package com.hiddenartist.backend.domain.mentor.persistence.repository;
 import com.hiddenartist.backend.domain.mentor.persistence.Mentor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MentorRepository extends JpaRepository<Mentor, Long> {
+public interface MentorRepository extends JpaRepository<Mentor, Long>, CustomMentorRepository {
 
 }
