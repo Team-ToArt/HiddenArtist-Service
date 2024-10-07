629dfd8d82c19068680cc2465e35b1a570440fa3
BlanC <74227460+kihong1602@users.noreply.github.com>
2024-10-07 21:07:50 +0900

    ♻️ Refactor: Mentoring 관련 테스트 추가 (#36)
    * ✅ Test: Mentoring 상세조회 테스트 구현
    * ✅ Test: MentoringApplication By Month 조회 테스트 구현
    * ♻️ Refactor: Distribute Lock 리팩토링 및 테스트 구현
      - 커스텀 어노테이션 @DistributedLock을 AOP를 통해 분산락이 작동하도록 구조 수정
      - RedisLockFacade, MentoringServiceWithLock 삭제 -> RedisLockClient로 수정해 컴포넌트 복잡도 감소
    * ✅ Test: Mentoring 신청시간 잠금 테스트 구현

--- backend/src/main/java/com/hiddenartist/backend/domain/mentoring/persistence/MentoringApplication.java
+++ backend/src/main/java/com/hiddenartist/backend/domain/mentoring/persistence/MentoringApplication.java
@@ -10,10 +10,12 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MentoringApplication extends BaseEntity {
@@ -30,5 +32,14 @@ public class MentoringApplication extends BaseEntity {
 
   @ManyToOne(fetch = FetchType.LAZY)
   private Account account;
+
+  public MentoringApplication(String token, LocalDateTime applicationTime, MentoringApplicationStatus mentoringApplicationStatus,
+      Mentoring mentoring, Account account) {
+    this.token = token;
+    this.applicationTime = applicationTime;
+    this.mentoringApplicationStatus = mentoringApplicationStatus;
+    this.mentoring = mentoring;
+    this.account = account;
+  }
 
 }
--- backend/src/test/java/com/hiddenartist/backend/global/config/AbstractMySQLRepositoryTest.java
+++ backend/src/test/java/com/hiddenartist/backend/global/config/AbstractMySQLRepositoryTest.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.containers.MySQLContainer;
 
 @DataJpaTest
 @Transactional
@@ -15,25 +14,9 @@ import org.testcontainers.containers.MySQLContainer;
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 public abstract class AbstractMySQLRepositoryTest {
 
-  private static final String MYSQL_IMAGE = "mysql:8.0.34";
-
-  private static final MySQLContainer<?> mysqlContainer = new MySQLContainer<>(MYSQL_IMAGE)
-      .withDatabaseName("pop")
-      .withUsername("test")
-      .withPassword("test")
-      .withCommand("--ft_min_word_len=2", "--innodb_ft_min_token_size=2")
-      .withInitScript("schema.sql");
-
-  static {
-    mysqlContainer.start();
-  }
-
   @DynamicPropertySource
   static void configureProperties(DynamicPropertyRegistry registry) {
-    System.out.println(mysqlContainer.getJdbcUrl());
-    registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
-    registry.add("spring.datasource.username", mysqlContainer::getUsername);
-    registry.add("spring.datasource.password", mysqlContainer::getPassword);
+    TestContainerSetup.setDynamicProperties(registry);
   }
 
 }
