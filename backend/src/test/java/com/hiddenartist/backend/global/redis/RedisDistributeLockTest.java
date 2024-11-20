cd966002da4d3c1799ee17fa16a706e388aa100f
BlanC <74227460+kihong1602@users.noreply.github.com>
2024-11-20 12:29:48 +0900

    🩹 Fix: Distributed Lock Test 수정 (#50)

--- backend/src/test/java/com/hiddenartist/backend/global/redis/RedisDistributeLockTest.java
+++ backend/src/test/java/com/hiddenartist/backend/global/redis/RedisDistributeLockTest.java
@@ -5,12 +5,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.hiddenartist.backend.domain.mentoring.service.MentoringService;
 import com.hiddenartist.backend.global.config.AbstractIntegrationTest;
 import com.hiddenartist.backend.global.exception.type.MentoringException;
+import com.hiddenartist.backend.global.exception.type.RedisLockException;
 import com.hiddenartist.backend.global.type.EntityToken;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,39 +24,34 @@ public class RedisDistributeLockTest extends AbstractIntegrationTest {
   private MentoringService mentoringService;
 
   @Test
-  @DisplayName("신청시간 잠금 테스트")
+  @DisplayName("Redis 분산 락으로 100개의 멘토링 신청 중 하나만 성공")
   void applicationTimeLockTest() {
     //given
-    ExecutorService executor = Executors.newFixedThreadPool(2);
+    ExecutorService executor = Executors.newFixedThreadPool(100);
 
-    String defaultEmail = "test@test.com";
-    String competitionEmail = "test2@test.com";
     String token = EntityToken.MENTORING.identifyToken("1");
     LocalDateTime applicationTime = LocalDateTime.of(2024, 10, 24, 15, 30);
 
+    AtomicInteger successCount = new AtomicInteger();
     AtomicInteger exceptionCount = new AtomicInteger();
-    //when
-    CompletableFuture<Void> request1 = CompletableFuture.runAsync(
-        () -> {
-          try {
-            mentoringService.reservationApplicationTime(token, applicationTime, defaultEmail);
-          } catch (MentoringException e) {
-            exceptionCount.getAndIncrement();
-          }
-        }, executor);
-    CompletableFuture<Void> request2 = CompletableFuture.runAsync(
-        () -> {
-          try {
-            mentoringService.reservationApplicationTime(token, applicationTime, competitionEmail);
-          } catch (MentoringException e) {
-            exceptionCount.getAndIncrement();
-          }
-        }, executor);
 
-    CompletableFuture.allOf(request1, request2).join();
+    List<CompletableFuture<Void>> requests = IntStream.range(0, 100).mapToObj(i ->
+                                                          CompletableFuture.runAsync(() -> {
+                                                            String email = "test" + i + "@test.com";
+                                                            try {
+                                                              mentoringService.reservationApplicationTime(token, applicationTime, email);
+                                                              successCount.getAndIncrement();
+                                                            } catch (MentoringException | RedisLockException e) {
+                                                              exceptionCount.getAndIncrement();
+                                                            }
+                                                          }, executor))
+                                                      .toList();
+    //when
+    CompletableFuture.allOf(requests.toArray(CompletableFuture[]::new)).join();
 
     //then
-    assertThat(exceptionCount.get()).isEqualTo(1);
+    assertThat(successCount.get()).isEqualTo(1);
+    assertThat(exceptionCount.get()).isEqualTo(99);
   }
 
 }
